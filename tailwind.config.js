186049adec306f238681a9358a16dd971ebf9943
moh1434 <moh1434.ma@gmail.com>
2021-09-20 01:58:33 +0300

    css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 module.exports = {
-    purge: {
-        content: ['./public/index.html']
-    },
+	purge: {
+		content: ['./public/index.html']
+	},
 
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            minWidth: {
-                '72': '18rem',
-            },
-        },
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			maxWidth: {
+				'8xl': "1300px",
+				'xs-p': '21rem',
+				'72': '18rem',
+			},
+			boxShadow: {
+				'my': '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)'
+			}
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 }
